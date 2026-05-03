39fdf12d3dccc8de509dfa729612d91c0507f1e2
Nagi <133611247+Nagi6055@users.noreply.github.com>
2026-05-03 18:08:53 -0500

    Fix formatting in hello_world.py docstring

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,9 @@
 """
 File: hello_world.py
-Description: A simple program that outputs the message “Hello, World!”
 Author: Nagi
 Date: 05-01-2026
+
+Description: A simple program that outputs the message “Hello, World!”
 """
 
 print("Hello, World!")
