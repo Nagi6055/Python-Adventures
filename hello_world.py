e5363ec98f1b4098f060d86ff50cda3d8285131a
Nagi <133611247+Nagi6055@users.noreply.github.com>
2026-05-03 18:09:59 -0500

    Enhance program description in hello_world.py
    Updated the description to include 'as a quick introduction to python programming'.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 """
+Project Name: Hello World
 File: hello_world.py
 Author: Nagi
 Date: 05-01-2026
 
-Description: A simple program that outputs the message “Hello, World!”
+Description: A simple program that outputs the message “Hello, World!” as a quick introduction to python programming.
 """
 
 print("Hello, World!")
