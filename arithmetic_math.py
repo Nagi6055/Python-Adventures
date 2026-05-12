da0cab53057ff34ff01008ca71cba02ae180c359
Nagi <133611247+Nagi6055@users.noreply.github.com>
2026-05-11 23:51:30 -0500

    Add files via upload

---
+++ arithmetic_math.py
@@ -0,0 +1,10 @@
+"""
+Project Name: Arithmetic & Math
+File Name: arithmetic_math.py
+Author: Nagi
+Date: 5/11/2026
+
+Description:
+    arithmetic operators, math functions, a few functions with the math module, and some exercises.
+
+"""
